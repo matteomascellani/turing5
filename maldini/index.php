2ddc9e0e77fc4423ed40297130a37b0c8f0ef276
Giulia Maldini <giuliamaldini@gmail.com>
2021-04-10 19:16:42 +0200

    primo commit

---
+++ maldini/index.php
@@ -0,0 +1,32 @@
+<?php
+
+define("V", 100);
+
+echo "Hello!";
+echo "<br />";
+$v = 1;
+echo $v;
+$v++;
+echo "<br />";
+echo $v;
+echo "<br />";
+$v += 3;
+echo $v;
+$v = $v + 3;
+echo "<br />";
+echo $v;
+echo "<br />";
+
+echo V;
+
+echo "<br />";
+$stringa = "Ciao io sono una stringa";
+echo $stringa;
+echo "<br />";
+
+$array = array("1",array(),3,4,5);
+$array2 = [1,2,3,4,5,6,7];
+
+print_r($array);
+var_dump($array);
+print_r($array2);
