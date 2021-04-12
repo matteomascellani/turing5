7f3704bf073fff2da93553d845ca576f712cf7dd
Matteo Mascellani <matteo.mascellani@gmail.com>
2021-04-12 11:47:23 +0200

    fix

--- mascellani/esercizio.php
+++ mascellani/esercizio.php
@@ -60,7 +60,7 @@ $risultato = $calcolatrice->result();
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 <style>
 
-
+.pushed { background-color:#000!important}
 
 </style>
 <script>
@@ -76,19 +76,22 @@ $( document ).ready(function() {
             $(this).val() == "div"
         ) {
 
-            $.ajax({
+            $("button").removeClass("pushed");
+            $(this).addClass("pushed");
+
+            /* $.ajax({
                 method: "POST",
                 url: "esercizio.php",
                 data: { operator: $(this).val() }
             })
             .done(function( data ) {
                 $("#risultato").val(data);
-            });
+            }); */
 
         } else {
+            $("button").removeClass("pushed");
             $("#risultato").val($(this).val());
         }
-
     });
 
 
