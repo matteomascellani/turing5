b4bb9b0360a88c41ff7b83dafeb254dbd507345f
RendinaD <damiano.rendina@gmail.com>
2021-04-13 14:35:31 +0200

    prima commit

---
+++ rendina/calcolatrice/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+</head>
+
+<script>
+
+</script>
+
+<body>
+
+</body>
