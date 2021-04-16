27a363962c291001cd24f2966e83fa173acc25e9
eliane temgoua <lytem85@gmail.com>
2021-04-16 11:15:18 +0200

    primo commit

---
+++ eliane/index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "<p> hello </p>";
+
+echo "hello eliane";
+
+
+$author = "John Doe";
+echo "<h1>Hello World!</h1> 
+<p>This dynamic web page was created by $author.</p>";
+
+$esempio = 'Hello ';
+$esempio .= 'World';
+echo $esempio;
+
+
+?>
