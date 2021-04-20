a03d66e2453be4ccfdf55c0b64284285562815a3
Giulia Maldini <giuliamaldini@gmail.com>
2021-04-20 22:46:52 +0200

    commit

---
+++ maldini/crud/database/migrations/2021_04_20_194733_create_music_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMusicTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('music', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('composer');
+            $table->integer('date');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('music');
+    }
+}
--- maldini/crud/routes/web.php
+++ maldini/crud/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MusicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::resource('music', MusicController::class);
 
 Route::get('/', function () {
     return view('welcome');
