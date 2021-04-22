1816fabbcc293278233479937d64362c777ff574
dguariento <davideguariento07@gmail.com>
2021-04-22 13:00:38 +0200

    Creazione azione Create e relativa View di inserimento dati

--- guariento/laravel/app/Models/Movie.php
+++ guariento/laravel/app/Models/Movie.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'titolo',
+        'descrizione',
+        'regia',
+        'anno',
+        'durata',
+        'genere'
+    ];
 }
