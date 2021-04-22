6cdf84f38508c394dc08b03305d425ceab709d6a
Eliane Temgoua <lytem85@gmailcom>
2021-04-22 03:06:47 +0200

    creazione tabella books in Db

--- eliane/laravel/routes/web.php
+++ eliane/laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('books',BookController::class);// il faut ajouter le parcours avec la use
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
