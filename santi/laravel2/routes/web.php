9e28c0ef1f1bba0d2b0fb5f5a3b710a197560c59
Francesco <francescosanti123@gmail.com>
2021-04-26 11:41:52 +0200

    santi

--- santi/laravel2/routes/web.php
+++ santi/laravel2/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\filmController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::resource('films', filmController::class);
 
 Route::get('/', function () {
     return view('welcome');
