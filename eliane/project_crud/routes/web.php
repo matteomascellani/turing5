9f1ad5b131665e1bb7d8f906560d3adece812e8c
Eliane Temgoua <lytem85@gmailcom>
2021-04-28 15:29:10 +0200

    commit

--- eliane/project_crud/routes/web.php
+++ eliane/project_crud/routes/web.php
@@ -17,3 +17,7 @@ Route::resource('films', FilmsController::class);
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
