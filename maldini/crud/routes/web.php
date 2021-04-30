ae194da9565807dc9178f9b7b37b260acc78ecea
Giulia Maldini <giuliamaldini@gmail.com>
2021-04-30 16:47:06 +0200

    crud

--- maldini/crud/routes/web.php
+++ maldini/crud/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MusicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+route::resource('musics', MusicController::class);
 
 Route::get('/', function () {
     return view('welcome');
