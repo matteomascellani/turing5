810b935cd095044b5702afaca1c56d321ebd49f9
Eliane Temgoua <lytem85@gmailcom>
2021-05-03 09:58:43 +0200

    creazione controller resource e model

--- eliane/libreria/routes/web.php
+++ eliane/libreria/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LibroController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::resource('libro', LibroController::class);
 Route::get('/', function () {
     return view('welcome');
 });
