006fa2782f1359a3dd77a0b50b17e3afd043ece8
leogaru <76592988+leogaru@users.noreply.github.com>
2021-05-05 00:01:42 +0200

    Create calcolatrice.php

---
+++ garuti/calcolatrice.php
@@ -0,0 +1,45 @@
+<?php
+
+?>
+
+<html>
+<head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+<style>
+
+</style>
+<script>
+</script>
+</head>
+
+<body>
+
+<h3>calcolatrice</h3>
+ 
+<div>La mia calcolatrice</div>
+
+<div class="container">
+    <table class="table table-bordered">
+    <tr>
+        <td>7</td>
+        <td>8</td>
+        <td>9</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>5</td>
+        <td>6</td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+    </tr>
+    </table>
+</div>
+
+</body>
+</html>
