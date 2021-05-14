b2930b1226d16220b734297420237e5cf6b86a4f
Francesco <francescosanti123@gmail.com>
2021-05-14 22:25:24 +0200

    Crud Laravel Gestione Prodotti funzionante

--- santi/prodottiLaravel/database/migrations/2021_05_14_195305_create_products_table.php
+++ santi/prodottiLaravel/database/migrations/2021_05_14_195305_create_products_table.php
@@ -15,6 +15,8 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('detail');
             $table->timestamps();
         });
     }
