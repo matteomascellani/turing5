9900f0be9e9beb231d0a1cb1836a78b7988354b8
Eliane Temgoua <lytem85@gmailcom>
2021-05-17 14:04:04 +0200

    creazione tabella professors, routes, create controller e route professor

---
+++ eliane/università/app/Models/Professor.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Professor extends Model
+{
+    use HasFactory;
+    protected $fillable=['nome','cognome','materia','email'];
+}
--- eliane/università/routes/web.php
+++ eliane/università/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\StudentsController;
+use App\Http\Controllers\ProfessorsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::resource('professors', ProfessorsController::class);
 Route::resource('students', studentsController::class);
 Route::get('/', function () {
     return view('welcome');
