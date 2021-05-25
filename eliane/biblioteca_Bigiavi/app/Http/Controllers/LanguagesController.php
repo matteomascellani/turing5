a4911f8bf1f244347aa010f9793b33e1c767baf0
Eliane Temgoua <lytem85@gmailcom>
2021-05-26 01:26:59 +0200

    implementazione curd controller language

---
+++ eliane/biblioteca_Bigiavi/app/Http/Controllers/LanguagesController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Language;
+use App\Http\Requests\LanguageRequest;
+class LanguagesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items=Language::get();
+        return view('language.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('language.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(LanguageRequest $request)
+    {
+        $language=new Language();
+        $language->create($request->input('language'));
+        return redirect('/languages');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Language $language)
+    {
+        return view('langguage.edit',compact('language'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Language $language)
+    {
+        $language->update($request->input('language'));
+        return redirect('/languages');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Language $language)
+    {
+        $language->delete($language);
+        return redirect('/languages');
+    }
+}
--- eliane/biblioteca_Bigiavi/routes/web.php
+++ eliane/biblioteca_Bigiavi/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LanguagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::resource('languages', LanguagesController::class);
