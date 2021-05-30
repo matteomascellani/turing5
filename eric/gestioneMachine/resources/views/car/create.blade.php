9b6d326667b9bb579b9dd4fc9ba888504bb4c7cc
eric christel <nzakerico@yahoo.it>
2021-05-30 12:49:52 +0200

    createFine

--- eric/gestioneMachine/resources/views/car/create.blade.php
+++ eric/gestioneMachine/resources/views/car/create.blade.php
@@ -6,12 +6,21 @@
             @csrf
             MARCA:<input type="text" name="car[marca]" value=""><br><br>
             COLORE:<input type="text" name="car[colore]" value=""><br><br>
-            NOME:<select name="car[customer_id]">
+            NOME CLIENTE:<select name="car[customer_id]">
                 @foreach ($customer as $custom)
                     <option value="{{$custom->id}}">{{$custom->nome}}</option>
                 @endforeach
-
-            </select>
+            </select><br><br>
+            COGNOME CLIENTE:<select>
+                @foreach ($customer as $custom)
+                    <option value="{{$custom->id}}">{{$custom->cognome}}</option>
+                @endforeach
+            </select><br><br>
+            PAESE CLIENTE:<select>
+                @foreach ($customer as $custom)
+                    <option value="{{$custom->id}}">{{$custom->paese}}</option>
+                @endforeach
+            </select><br><br>
             <input type="submit" value="crea">
         </form>
         <a href="/cars">lista macchine disponibili</a>
