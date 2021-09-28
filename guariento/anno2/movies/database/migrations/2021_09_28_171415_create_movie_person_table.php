6f8d601aa727e10d040199dce2aa583d0588ddbe
dguariento <davideguariento07@gmail.com>
2021-09-28 19:22:36 +0200

    Creazione migrations

---
+++ guariento/anno2/movies/database/migrations/2021_09_28_171415_create_movie_person_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviePersonTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movie_person', function (Blueprint $table) {
+            $table->id();
+            $table->integer('movie_id');
+            $table->integer('person_id');
+            $table->integer('type_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movie_person');
+    }
+}
