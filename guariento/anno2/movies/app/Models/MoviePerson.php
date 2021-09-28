cf79d57b413eac780f92a6cd0b4e42330b224c5e
dguariento <davideguariento07@gmail.com>
2021-09-28 19:25:01 +0200

    Creazione models

---
+++ guariento/anno2/movies/app/Models/MoviePerson.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MoviePerson extends Model
+{
+    use HasFactory;
+}
